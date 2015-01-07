83134d280cd011ca35e2b578a6fc9835ef46666e
Absalom <absalomaraujo@gmail.com>
2015-01-06 22:30:28 -0600

    changes from master

---
+++ src/java/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author absalom
+ */
+public class NewClass {
+
+    public NewClass() {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                
+            }
+        }
+    }
+    
+}
